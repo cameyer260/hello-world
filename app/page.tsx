42fa0de45384166cf5b0740270e35b2fc5bceac1
Christopher Meyer <cameyer06@gmail.com>
2025-10-09 10:44:50 -0500

    added some extra ui, shelling out a sample submit form to figure out how to connect to docker db instance w/ postgres

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,36 @@
+"use client";
+
+import { useState } from "react";
+
 import Link from "next/link";
 
 export default function Home() {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+
+  async function submitForm(e: React.FormEvent) {
+    e.preventDefault();
+
+    const res = await fetch("/api/submit", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email }),
+    });
+
+    if (res.ok) {
+      setMessage("✅ Form submitted successfully!");
+      setName("");
+      setUsername("");
+      setEmail("");
+    } else {
+      setMessage("❌ Error submitting form.")
+      setError(true);
+    }
+  }
+
   return (
     <div className="flex flex-col justify-center text-3xl p-6 gap-4">
       <div className="flex justify-center">
@@ -19,6 +49,40 @@ export default function Home() {
           <u>playskillsphere.com</u>
         </Link>
         <h1>just adding a useless change so i can test deploy on vps</h1>
+      </div>
+
+      <div className="border-t mt-6">
+        <h1 className={`${error ? "bg-red-800" : "bg-green-800"} h-10px text-center`}>{message}</h1>
+        <form
+          onSubmit={submitForm}
+          className="flex flex-col [&>input]:w-1/2 items-center gap-4 pt-4"
+        >
+          <h1>Put your name here!</h1>
+          <input
+            type="text"
+            placeholder="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="p-2 border rounded"
+          />
+          <input
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="p-2 border rounded"
+          />
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-2 border rounded"
+          />
+          <button type="submit" className="p-2 border rounded">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
